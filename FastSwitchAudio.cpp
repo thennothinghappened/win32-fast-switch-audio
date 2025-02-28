24ac67cf4db82e4e4f2086e48a85070c035b475a
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2025-03-01 10:22:55 +1100

    refactor: WIP menu rewrite
    This is not a functional commit!
    
    Saving this here for reference. This is not exactly the direction I am planning now, but it was a useful exploration.

--- FastSwitchAudio.cpp
+++ FastSwitchAudio.cpp
@@ -9,6 +9,7 @@
 #include <ranges>
 #include <windowsx.h>
 #include <shellapi.h>
+#include <cassert>
 #include "Audio/Audio.h"
 #include "Audio/DeviceManagerImpl.h"
 
@@ -23,22 +24,24 @@
 	language='*'											\
 \"")
 
+enum UserMessage
+{
+	WM_ToggleTrayIcon = (WM_USER + 1)
+};
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
 /**
  * @brief Maximum size we're allocating for strings when using `LoadStringW`.
  */
 constexpr uint32_t maxLoadString = 128;
 
-enum class Notification : uint32_t
-{
-	ToggleTrayIcon = (WM_USER + 0x100)
-};
+static Audio::DeviceManagerImpl audioDeviceManager;
 
-Audio::DeviceManagerImpl audioDeviceManager;
+static HWND trayWindow;
+static WCHAR windowClassName[maxLoadString];
 
-HWND trayWindow;
-WCHAR windowClassName[maxLoadString];
-
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+static HMENU popupMenu = CreatePopupMenu();
 
 int APIENTRY wWinMain(
 	_In_ HINSTANCE hInstance,
@@ -92,15 +95,17 @@ int APIENTRY wWinMain(
 		.hWnd = trayWindow,
 		.uID = 1,
 		.uFlags = NIF_MESSAGE | NIF_ICON,
-		.uCallbackMessage = static_cast<UINT>(Notification::ToggleTrayIcon),
+		.uCallbackMessage = WM_ToggleTrayIcon,
 		.hIcon = LoadIconW(hInstance, MAKEINTRESOURCE(IDI_SMALL)),
 		.uVersion = NOTIFYICON_VERSION_4
 	};
 	
-	if (Shell_NotifyIcon(NIM_ADD, &trayIconData))
+	if (Shell_NotifyIconW(NIM_ADD, &trayIconData) == false)
 	{
-		Shell_NotifyIcon(NIM_SETVERSION, &trayIconData);
+		return FALSE;
 	}
+
+	Shell_NotifyIconW(NIM_SETVERSION, &trayIconData);
 
 	HACCEL acceleratorTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_FASTSWITCHAUDIO));
 	MSG msg;
@@ -117,70 +122,187 @@ int APIENTRY wWinMain(
 	return (int)msg.wParam;
 }
 
+/**
+ * @brief Identifying data for a given menu item in the popup menu.
+ */
+using MenuItem = struct
+{
 
+	enum class Type : UCHAR
+	{
+		AudioDevice,
+		Separator,
+		ExitButton,
+	};
+
+	/**
+	 * @brief The type of item this is.
+	 */
+	const Type type;
+
+	/**
+	 * @brief Unique
+	 */
+	const UCHAR id;
+
+	/**
+	 * @brief Data associated with this item.
+	 */
+	const union
+	{
+		/**
+		 * @brief ID of the audio device this item is representing.
+		 */
+		USHORT AudioDevice_id;
+
+		/**
+		 * @brief Empty padding data for self-explanatory item types.
+		 */
+		USHORT filler;
+	} data;
+
+	/**
+	 * @brief Pack this item into the form Windows provides for storing menu item data.
+	 * @return An unsigned integer that contains the same data this item is storing.
+	 */
+	const UINT pack() const
+	{
+		UINT packed;
+		std::memcpy(&packed, this, sizeof(UINT));
+
+		return packed;
+	}
+
+	/**
+	 * @brief Unpack this item from the form provided by Windows.
+	 * @param packed The packed data to be reinterpreted as a menu item.
+	 * @return The represented item.
+	 */
+	static const MenuItem unpack(UINT packed)
+	{
+		MenuItem item{};
+		std::memcpy(&packed, &item, sizeof(UINT));
+
+		return item;
+	}
+
+	static_assert(sizeof(MenuItem) == sizeof(UINT), "Menu items must be storable within the provided `UINT` we're given to ID them with.");
+};
+
+
+static MENUITEMINFOW createMenuSeparator(UINT id)
+{
+	MenuItem item{
+		.type = MenuItem::Type::Separator
+	};
+
+	return MENUITEMINFOW{
+		.cbSize = sizeof(MENUITEMINFOW),
+		.fMask = MIIM_ID | MIIM_TYPE | MIIM_DATA,
+		.fType = MFT_SEPARATOR,
+		.dwItemData = item.pack()
+	};
+}
+
+static void initPopupMenu()
+{
+	UINT menuIndex = 1;
+
+	for (UINT deviceIndex = 0; deviceIndex < audioDeviceManager.count(); deviceIndex++)
+	{
+		const Audio::Device& device = audioDeviceManager[deviceIndex];
+		std::wstring itemLabel = std::wstring(device.getName());
+
+		MENUITEMINFOW item{
+			.cbSize = sizeof(MENUITEMINFOW),
+			.fMask = MIIM_STRING | MIIM_STATE | MIIM_ID | MIIM_DATA,
+			.fState = MFS_ENABLED,
+			.wID = menuIndex,
+			.dwItemData = MenuItem{
+				.type = MenuItem::Type::AudioDevice,
+				.data = static_cast<USHORT>(deviceIndex)
+			}.pack(),
+			.dwTypeData = itemLabel.data()
+		};
+
+		InsertMenuItemW(popupMenu, menuIndex, TRUE, &item);
+		menuIndex++;
+	}
+
+
+	MENUITEMINFOW separatorItem = createMenuSeparator(menuIndex);
+	InsertMenuItemW(popupMenu, menuIndex, TRUE, &separatorItem);
+	menuIndex++;
+
+	MENUITEMINFOW exitItem{
+		.cbSize = sizeof(MENUITEMINFOW),
+		.fMask = MIIM_STRING | MIIM_STATE | MIIM_ID | MIIM_DATA,
+		.fState = MFS_ENABLED,
+		.wID = menuIndex,
+		.dwItemData = MenuItem{
+			.type = MenuItem::Type::ExitButton
+		}.pack(),
+		.dwTypeData = std::wstring(L"Exit").data()
+	};
+
+	InsertMenuItemW(popupMenu, menuIndex, TRUE, &exitItem);
+	menuIndex++;
+}
+
+static void showPopupMenu()
+{
+	SetForegroundWindow(trayWindow);
+
+	POINT cursor;
+	GetCursorPos(&cursor);
+
+	constexpr UINT popupFlags = TPM_LEFTALIGN | TPM_LEFTBUTTON | TPM_RIGHTBUTTON | TPM_RETURNCMD;
+	UINT itemId = TrackPopupMenu(popupMenu, popupFlags, cursor.x, cursor.y, 0, trayWindow, NULL);
+
+	if (itemId == 0)
+	{
+		return;
+	}
+
+	if (itemId & 0b1000000)
+	{
+		uint32_t deviceId = itemId ^ 0b1000000;
+		audioDeviceManager[deviceId].setAsDefault();
+	}
+}
 
 LRESULT CALLBACK WndProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	static HMENU menu = CreatePopupMenu();
-
 	switch (message)
 	{
-
 		case WM_CREATE:
 		{
-			for (uint32_t i = 0; i < audioDeviceManager.count(); i++)
-			{
-				const Audio::Device& device = audioDeviceManager[i];
-				std::wstring itemLabel = std::wstring(device.getName());
-
-				MENUITEMINFOW item{
-					.cbSize = sizeof(MENUITEMINFOW),
-					.fMask = MIIM_STRING | MIIM_STATE | MIIM_ID,
-					.fState = MFS_ENABLED,
-					.wID = i | 0b1000000, // FIXME: evil magic!!
-					.dwTypeData = itemLabel.data()
-				};
-
-				InsertMenuItemW(menu, i, TRUE, &item);
-			}
-
+			initPopupMenu();
 			break;
 		}
 
-		case static_cast<UINT>(Notification::ToggleTrayIcon):
+		case WM_ToggleTrayIcon:
 		{
 			switch (LOWORD(lParam))
 			{
 				case WM_RBUTTONDOWN:
 				{
-					POINT cursorPos;
-					GetCursorPos(&cursorPos);
-
-					SetForegroundWindow(trayWindow);
-
-					constexpr uint32_t popupFlags = TPM_LEFTALIGN | TPM_LEFTBUTTON | TPM_RIGHTBUTTON | TPM_RETURNCMD;
-					const uint32_t itemId = TrackPopupMenu(menu, popupFlags, cursorPos.x, cursorPos.y, 0, trayWindow, NULL);
-
-					if (itemId & 0b1000000)
-					{
-						const uint32_t deviceId = itemId ^ 0b1000000;
-						audioDeviceManager[deviceId].setAsDefault();
-
-						break;
-					}
-
+					showPopupMenu();
 					break;
 				}
-			}
 
-			return DefWindowProcW(window, message, wParam, lParam);
+				default:
+				{
+					return DefWindowProcW(window, message, wParam, lParam);
+				}
+			}
+			break;
 		}
 
 		default:
 		{
 			return DefWindowProcW(window, message, wParam, lParam);
 		}
-
 	}
 
 	return 0;
