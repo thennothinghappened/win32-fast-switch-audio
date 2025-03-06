fbdf4e378edf733a36be632db6d1ddc545b6f5cb
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2025-03-07 10:07:21 +1100

    feat: add nice debugging macro

--- utils.h
+++ utils.h
@@ -3,6 +3,12 @@
 #include "framework.h"
 #include <string>
 
+#ifdef _DEBUG
+#define DEBUG_WRITE(MESSAGE) OutputDebugStringW(std::format(L"{} {} line {}: {}\n", (__FILEW__), (__FUNCTIONW__), (__LINE__), (MESSAGE)).c_str())
+#else
+#define DEBUG_WRITE(MESSAGE)
+#endif
+
 /**
  * @brief Show a fatal error message box if something went wrong we cannot recover from.
  * @param message The message to be shown.
