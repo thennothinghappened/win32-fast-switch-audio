e027cc42177aac9ed35d5122cf2ad5d502148669
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2025-03-07 15:31:00 +1100

    fix: use case-sensitive name of `sdkddkver.h`

--- src/targetver.h
+++ src/targetver.h
@@ -5,4 +5,4 @@
 // set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
 // #include <WinSDKVer.h>
 // #define _WIN32_WINNT _WIN32_WINNT_WIN7
-#include <SDKDDKVer.h>
+#include <sdkddkver.h>
